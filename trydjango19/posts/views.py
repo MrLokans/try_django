c2b9ba94ef693af662d216506b0c7e0b52acb08c
MrLokans <trikster1911@gmail.com>
2016-03-27 13:52:52 +0300

    Detail view added. Relative urls are now used in the list view.

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+def post_detail(request, id=None):
+
+    post = get_object_or_404(Post, id=id)
+
+    return render(request, "post_detail.html", {"post": post})
 
 
 def post_list(request):
-    return render(request, "index.html", {})
+    posts = Post.objects.all()
+    return render(request, "index.html", {"posts": posts})
 
 
 def post_update(request):
