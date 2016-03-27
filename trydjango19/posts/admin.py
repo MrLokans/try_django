f036933193c65f122085a1576934cc90740bbcd8
MrLokans <trikster1911@gmail.com>
2016-03-27 12:35:57 +0300

    Admin management for the Post model added.

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+
+    list_display = ("title", "timestamp", "updated")
+    list_display_links = ["updated"]
+    list_editable = ["title"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
+
+
+admin.site.register(Post, PostModelAdmin)
