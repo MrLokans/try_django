4f4314305d31aabef8a8b8e4febd9ba78a067148
MrLokans <trikster1911@gmail.com>
2016-03-27 13:58:44 +0300

    Reverse method is now used to obtain post's url.

--- trydjango19/posts/models.py
+++ trydjango19/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -12,3 +13,6 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.__str__()
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={'id': id})
