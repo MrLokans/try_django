8a0ec1669f5224b7ba7f338202c8f2a87808da2b
MrLokans <trikster1911@gmail.com>
2016-04-03 10:09:37 +0300

    Server-side markdown rendering added.

--- trydjango19/posts/models.py
+++ trydjango19/posts/models.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
+
+from markdown_deux import markdown
 
 
 class PostManager(models.Manager):
@@ -55,6 +58,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs={'slug': self.slug})
+
+    def get_markdown(self):
+        content = self.content
+        return mark_safe(markdown(content))
 
 
 def create_slug(instance, new_slug=None):
--- trydjango19/trydjango19/settings.py
+++ trydjango19/trydjango19/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # thirdparty
     'pagedown',
+    'markdown_deux',
 
     # local
     'posts',
