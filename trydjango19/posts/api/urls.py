6c1c9794c05fecacf3cd91e7f8c7f5660f9d0e6a
MrLokans <trikster1911@gmail.com>
2016-04-30 10:07:31 +0300

    Retrieve view to display single post data added.

--- trydjango19/posts/api/urls.py
+++ trydjango19/posts/api/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 
 from posts.api.views import (
     PostListAPIView,
+    PostDetailAPIView,
 )
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name="list"),
+    # restframework treats pk as the default search value
+    url(r'^(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name="detail"),
     # url(r'^create/$', post_create),
-    # url(r'^(?P<slug>[\w-]+)/$', post_detail, name="detail"),
     # url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name="update"),
     # url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name="delete"),
 ]
--- trydjango19/posts/api/views.py
+++ trydjango19/posts/api/views.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 
 
 class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
