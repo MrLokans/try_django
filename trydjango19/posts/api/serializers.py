27e15635b6ba355d07ce904ca40aeebed5f9cf23
MrLokans <trikster1911@gmail.com>
2016-05-01 20:21:07 +0300

    Custom method serializer added to return post author name and image url.

--- trydjango19/posts/api/serializers.py
+++ trydjango19/posts/api/serializers.py
@@ -9,28 +9,56 @@ class PostListSerializer(serializers.ModelSerializer):
         view_name='posts-api:detail',
         lookup_field='pk'
     )
+    user = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = [
             'url',
             'id',
+            'image',
+            'user',
             'title',
         ]
 
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image_url = obj.image.url
+        except:
+            image_url = None
+        return image_url
+
 
 class PostDetailSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = [
             'user',
             'id',
+            'image',
             'title',
             'slug',
             'content',
             'publish',
             'updated'
         ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image_url = obj.image.url
+        except:
+            image_url = None
+        return image_url
 
 
 class PostCreateUpdateSerializer(serializers.ModelSerializer):
