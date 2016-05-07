2c45b8ec21f5d8636040ecf34db29ef75b0b6313
MrLokans <trikster1911@gmail.com>
2016-05-07 09:37:52 +0300

    Added function to create CommentCreateSerializer class.

--- trydjango19/comments/api/serializers.py
+++ trydjango19/comments/api/serializers.py
@@ -1,6 +1,42 @@
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
 from comments.models import Comment
+
+
+def create_comment_serializer(model_type='post', id=None, parent_id=None):
+    class CommentCreateSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = [
+                'id',
+                'content',
+                'parent',
+                'timestamp'
+            ]
+
+        def __init__(self, *args, **kwargs):
+            self.type = model_type
+            self.id = id
+            self.parent_obj = None
+            if self.parent_id:
+                parent_qs = Comment.objects.filter(id=parent_id)
+                if parent_qs.exists() and parent_qs.count() == 1:
+                    self.parent_obj = parent_qs.first()
+            return super().__init__(*args, **kwargs)
+
+        def validate(self, data):
+            model_type = self.model_type
+            model_qs = ContentType.object.filter(model=model_type)
+            if not model_qs.exists() or model_qs.count() != 1:
+                raise serializers.Validationerror("Invalid content type.")
+            SomeModel = model_qs.first().model_class()
+            obj_qs = SomeModel.objects.filter(id=self.id)
+            if not obj_qs.exists() or obj_qs.count() != 1:
+                raise serializers.Validationerror("Invalid content type id.")
+            return data
+
+    return CommentCreateSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
